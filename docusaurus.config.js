0845346dcb8c889e1f0caff09c0847a0bfc90e4b
Gan Kok Koon <kkgan@Gans-MacBook-Pro.local>
2024-02-17 15:20:52 +0800

    Changed config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,6 +79,10 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 5,
+      },
     }),
 };
 
